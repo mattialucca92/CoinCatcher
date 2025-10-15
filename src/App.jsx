dc719411db4673174d1468ecca682f3241bc5338
mattialucca92 <mattialucca92@gmail.com>
2025-10-15 18:56:13 +0200

    modifiche

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,42 @@
-
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, HashRouter, Route, Navigate } from "react-router-dom";
 import AppLayout from "./layout/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Transaction from "./pages/Transaction";
-import { useState,useEffect } from "react";
-
+import { useState, useEffect } from "react";
 
 function App() {
-    const [transazioni, setTransazioni] = useState(() => {
-      const saved = localStorage.getItem("transazioni");
-      return saved ? JSON.parse(saved) : [];
-    });
+  const [transazioni, setTransazioni] = useState(() => {
+    const saved = localStorage.getItem("transazioni");
+    return saved ? JSON.parse(saved) : [];
+  });
 
   useEffect(() => {
     localStorage.setItem("transazioni", JSON.stringify(transazioni));
   }, [transazioni]);
 
-
   return (
     <>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Navigate to="/dashboard" />} />
-          <Route
-            path="dashboard"
-            element={<Dashboard transazioni={transazioni} />}
-          />
-          <Route
-            path="transactions"
-            element={
-              <Transaction
-                transazioni={transazioni}
-                setTransazioni={setTransazioni}
-              />
-            }
-          />
-        </Route>
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate to="/dashboard" />} />
+            <Route
+              path="dashboard"
+              element={<Dashboard transazioni={transazioni} />}
+            />
+            <Route
+              path="transactions"
+              element={
+                <Transaction
+                  transazioni={transazioni}
+                  setTransazioni={setTransazioni}
+                />
+              }
+            />
+          </Route>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
