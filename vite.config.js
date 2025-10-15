70b9b5cae437bd386a34272979d269af1bef4b9a
mattialucca92 <mattialucca92@gmail.com>
2025-10-15 19:20:44 +0200

    modifica

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/CoinCatcher/", // <-- questo è fondamentale
+  base: "/", // <-- questo è fondamentale
   plugins: [react(), tailwindcss()],
 });
